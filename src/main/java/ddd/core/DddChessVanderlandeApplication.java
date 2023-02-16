6307e597bfce1d03606ec73ca41b83ad8266af26
Mohammad Hussein <mohammad.hussein@vanderlande.com>
2023-02-16 14:26:04 +0100

    Project setup: * Add spring boot and necessary dependencies

---
+++ src/main/java/ddd/core/DddChessVanderlandeApplication.java
@@ -0,0 +1,13 @@
+package ddd.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DddChessVanderlandeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DddChessVanderlandeApplication.class, args);
+	}
+
+}

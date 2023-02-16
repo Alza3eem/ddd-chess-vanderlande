54208fd87464cbc4e12d443c44d063b22a5e2310
Mohammad Hussein <mohammad.hussein@vanderlande.com>
2023-02-16 14:27:17 +0100

    Project setup: * Add spring boot and necessary dependencies

---
+++ src/test/java/ddd/core/DddChessVanderlandeApplicationTests.java
@@ -0,0 +1,13 @@
+package ddd.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DddChessVanderlandeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

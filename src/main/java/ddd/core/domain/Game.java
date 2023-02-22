aa21825a56bcd3fa714b36eece682df498a3c854
dece <Christopher.Eulering@vanderlande.com>
2023-02-22 12:03:44 +0100

    Added constructor for game

--- src/main/java/ddd/core/domain/Game.java
+++ src/main/java/ddd/core/domain/Game.java
@@ -1,10 +1,22 @@
 package ddd.core.domain;
 
-public class Game extends Entity<> {
+public class Game extends Entity {
 
+    GameId gameId;
     Player playerWhite;
     Player playerBlack;
+    Player currentPlayer;
     Board board;
 
-    //Todo: Proceed from here
+    public Game(GameId gameId, Player playerWhite, Player playerBlack) {
+        super(gameId);
+        this.playerWhite = playerWhite;
+        this.playerBlack = playerBlack;
+        currentPlayer = playerWhite;
+        board = new Board(gameId);
+    }
+
+    public void tryMovePiece(Player player, Position startPosition, Position endPosition) {
+
+    }
 }
